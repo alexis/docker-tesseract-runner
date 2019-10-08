4aac0337b9f36d2ee36edcf3f5fd0fa37fe59c71
Alexey Smolianinov <alexisowl@gmail.com>
2019-10-08 18:49:13 +0300

    minor updates and fixes

--- docker-tesseract.sh
+++ docker-tesseract.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Usage:
+# TESLANG=rus+eng ./docker-tesseract.sh /path/to/file.jpg
+
 INPUTFILE=$(cd $(dirname "$1") && pwd -P)/$(basename "$1")
 INPUT_BASENAME=$(basename $INPUTFILE)
 
@@ -9,5 +12,6 @@ docker-compose -f "$(dirname $0)/docker-compose.yml"  \
   run --rm \
   -v "$INPUTFILE":/tmp/"$INPUT_BASENAME" \
   -v "$OUTPUTPATH"/tesseract-output:/tmp/output \
+  -e "TESLANG=$TESLANG" \
   runner \
-  ./tesseract.sh "/tmp/$INPUT_BASENAME" "/tmp/output/$INPUT_BASENAME"
+  /project/tesseract.sh "/tmp/$INPUT_BASENAME" "/tmp/output/$INPUT_BASENAME"
